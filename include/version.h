45f0013ec1b61b87b271cbe0e71ee11d6333d43e
ermiry <erick.salas@ermiry.com>
2021-02-27 21:10:25 -0600

    Added dedicated version header

---
+++ include/version.h
@@ -0,0 +1,10 @@
+#ifndef _PACMAN_VERSION_H_
+#define _PACMAN_VERSION_H_
+
+#define PACMAN_VERSION					"0.1"
+#define PACMAN_VERSION_NAME				"Version 0.1"
+#define PACMAN_VERSION_DATE				"27/02/2021"
+#define PACMAN_VERSION_TIME				"21:10 CST"
+#define PACMAN_VERSION_AUTHOR			"Erick Salas"
+
+#endif
