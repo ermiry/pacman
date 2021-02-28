b58eded387184fb1cbd74b414a68b9acafe33269
ermiry <erick.salas@ermiry.com>
2021-02-27 21:10:45 -0600

    Added base types in dedicated header

---
+++ include/types.h
@@ -0,0 +1,19 @@
+#ifndef _PACMAN_TYPES_H_
+#define _PACMAN_TYPES_H_
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef unsigned char uchar;
+typedef unsigned char ascii_char;
+
+#endif
